fde62fd3c716299a6064f85d7e7d65be8c05f8db
Daniel <dshteinbok@inscopix.com>
2022-12-01 17:32:51 -0800

    did some more experimentation with nearest-neighbor, added a line to make the rotational mastermat

--- common/make_mastermat.py
+++ common/make_mastermat.py
@@ -19,9 +19,17 @@ obj_dims = (640, 1024)
         #savepath = ("row_inds_lin_csr.npy", "col_inds_lin_csr.npy", "values_lin_csr.npy"), 
         #w_interp_method="linear")
 
+#mastermat.make_mastermat_save_homemade(psfs_directory, psf_meta_path, img_dims, obj_dims, 
+        #savepath = ("row_inds_curve_csr.npy", "col_inds_curve_csr.npy", "values_curve_csr.npy"), 
+        #w_interp_method="cubic")
+
+#mastermat.make_mastermat_save_homemade(psfs_directory, psf_meta_path, img_dims, obj_dims, 
+        #savepath = ("row_inds_curve_nearest_csr.npy", "col_inds_curve_nearest_csr.npy", "values_curve_nearest_csr.npy"), 
+        #w_interp_method="nearest")
+
 mastermat.make_mastermat_save_homemade(psfs_directory, psf_meta_path, img_dims, obj_dims, 
-        savepath = ("row_inds_curve_csr.npy", "col_inds_curve_csr.npy", "values_curve_csr.npy"), 
-        w_interp_method="cubic")
+        savepath = ("row_inds_curve_rot_csr.npy", "col_inds_curve_rot_csr.npy", "values_curve_rot_csr.npy"), 
+        w_interp_method="cubic", rotate_psfs=True)
 
 #mastermat.make_mastermat_save_homemade(psfs_directory, psf_meta_path, img_dims, obj_dims, 
         #savepath = ("row_inds_diy_csr.npy", "col_inds_diy_csr.npy", "values_diy_csr.npy"), 
