7850a7c1adb67e940da5bd9d1b4d872958f403ba
Daniel <dshteinbok@inscopix.com>
2023-03-29 11:56:51 -0700

    changed reference to data in my profile to data in the data folder

--- common/make_mastermat.py
+++ common/make_mastermat.py
@@ -1,23 +1,23 @@
 import mastermat
 
-#psfs_directory = "/home/dshteinbok/nV3_PSFs_flat_hd"
-#psfs_directory = "/home/dshteinbok/nV3_PSFs_curve"
-#psfs_directory = "/home/dshteinbok/nV3_PSFs_flat"
-#psfs_directory = "/home/dshteinbok/nV3_PSFs_probe"
-#psfs_directory = "/home/dshteinbok/nV3_PSFs_probe_mark"
-#psfs_directory = "/home/dshteinbok/nV3_PSFs_probe_mark_green"
-#psfs_directory = "/home/dshteinbok/nV3_PSFs_probe_depth1"
-#psfs_directory = "/home/dshteinbok/nV3_PSFs_probe_depth2"
-#psfs_directory = "/home/dshteinbok/nV3_PSFs_probe_depth3"
-#psfs_directory = "/home/dshteinbok/nV3_PSFs_tilted"
-#psf_meta_path = "/home/dshteinbok/nV3_PSFs_flat_meta/metafile_hd.csv"
-#psf_meta_path = "/home/dshteinbok/nV3_PSFs_meta/metafile.csv"
-#psf_meta_path = "/home/dshteinbok/nV3_PSFs_flat_meta/metafile.csv"
-#psf_meta_path = "/home/dshteinbok/nV3_PSFs_flat_meta/metafile_probe_mark.csv"
-#psf_meta_path = "/home/dshteinbok/nV3_PSFs_probe_depths/metafile_probe_depth1.csv"
-#psf_meta_path = "/home/dshteinbok/nV3_PSFs_probe_depths/metafile_probe_depth2.csv"
-#psf_meta_path = "/home/dshteinbok/nV3_PSFs_probe_depths/metafile_tilted.csv"
-#psf_meta_path = "/home/dshteinbok/nV3_PSFs_flat_meta/metafile_probe.csv"
+#psfs_directory = "../data/nV3_PSFs_flat_hd"
+#psfs_directory = "../data/nV3_PSFs_curve"
+#psfs_directory = "../data/nV3_PSFs_flat"
+#psfs_directory = "../data/nV3_PSFs_probe"
+#psfs_directory = "../data/nV3_PSFs_probe_mark"
+#psfs_directory = "../data/nV3_PSFs_probe_mark_green"
+#psfs_directory = "../data/nV3_PSFs_probe_depth1"
+#psfs_directory = "../data/nV3_PSFs_probe_depth2"
+#psfs_directory = "../data/nV3_PSFs_probe_depth3"
+#psfs_directory = "../data/nV3_PSFs_tilted"
+#psf_meta_path = "../data/nV3_PSFs_flat_meta/metafile_hd.csv"
+#psf_meta_path = "../data/nV3_PSFs_meta/metafile.csv"
+#psf_meta_path = "../data/nV3_PSFs_flat_meta/metafile.csv"
+#psf_meta_path = "../data/nV3_PSFs_flat_meta/metafile_probe_mark.csv"
+#psf_meta_path = "../data/nV3_PSFs_probe_depths/metafile_probe_depth1.csv"
+#psf_meta_path = "../data/nV3_PSFs_probe_depths/metafile_probe_depth2.csv"
+#psf_meta_path = "../data/nV3_PSFs_probe_depths/metafile_tilted.csv"
+#psf_meta_path = "../data/nV3_PSFs_flat_meta/metafile_probe.csv"
 img_dims = (800, 1280)
 obj_dims = (640, 1024)
 
@@ -85,8 +85,8 @@ obj_dims = (640, 1024)
         #savepath = ("row_inds_probe_undistorted_tilted_csr.npy", "col_inds_probe_undistorted_tilted_csr.npy", "values_probe_undistorted_tilted_csr.npy"),
         #w_interp_method="cubic", avg_nnz=2000, original_shift=False)
 
-mastermat.make_mastermat_save_homemade("/home/dshteinbok/nV3_PSFs_probe_mark_green",
-                                       "/home/dshteinbok/nV3_PSFs_flat_meta/metafile_probe_mark.csv",
+mastermat.make_mastermat_save_homemade("../data/nV3_PSFs_probe_mark_green",
+                                       "../data/nV3_PSFs_flat_meta/metafile_probe_mark.csv",
                                        img_dims, obj_dims,
         savepath = ("row_inds_probe_green_undistorted_corrected_csr.npy", "col_inds_probe_green_undistorted_corrected_csr.npy", "values_probe_green_undistorted_corrected_csr.npy"),
         w_interp_method="cubic", avg_nnz=1000, original_shift=True)
