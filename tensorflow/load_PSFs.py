364f156c895f5316863656346cb237ad3db695fb
Daniel <dshteinbok@inscopix.com>
2023-04-25 10:04:21 -0700

    added ability to manage the strehl ratios of the PSFs

--- tensorflow/load_PSFs.py
+++ tensorflow/load_PSFs.py
@@ -32,7 +32,7 @@ class MetaMan:
             shifts[num] = [int(float(row["X image (px)"])), int(float(row["Y image (px)"]))]
             #shifts[num] = [-int(float(row["X image (px)"])), -int(float(row["Y image (px)"]))]
         return shifts
-    
+
     @property
     def field_origins(self):
         """
@@ -46,13 +46,23 @@ class MetaMan:
             # invert the origins... this is a LIE! but a necessary one for now...
 #             origins[num] = [float(row["X (mm)"])*-1000, float(row["Y (mm)"])*-1000]
         return origins
-    
+
+    @property
+    def strehl_ratios(self):
+        """
+        Get the Strehl ratio of the associated field
+        """
+        ratios = {}
+        for row in self.data:
+            num = int(row["Field Number"])
+            ratios[num] = float(row["Strehl ratio"])
+        return ratios
 
 def pad_to_position(psf, centerpoint, img_dims):
     """
     zero-pad a PSF so its centerpoint ends up at the centerpoint coordinate
     relative to the center of the image
-    
+
     Parameters:
         psf: np.ndarray
         centerpoint: tuple (int x, int y)
@@ -62,7 +72,7 @@ def pad_to_position(psf, centerpoint, img_dims):
     """
     hdiff = img_dims[0] - psf.shape[0]
     wdiff = img_dims[1] - psf.shape[1]
-    
+
     # In the future, these could be used in another way
     # Now, I'm just catching the error and discarding the image
     # A much better way would be to just put all of these in a single if statement with 'or' between them
@@ -74,7 +84,7 @@ def pad_to_position(psf, centerpoint, img_dims):
         raise ValueError("PSF falls off the bottom side of image field")
     if np.math.ceil(wdiff/2) + centerpoint[0] < 16:
         raise ValueError("PSF falls off the top side of image field")
-    
+
     return np.pad(psf,
                               ((np.math.ceil(hdiff/2) + centerpoint[1], np.math.floor(hdiff/2) - centerpoint[1]),
                               (np.math.ceil(wdiff/2) + centerpoint[0], np.math.floor(wdiff/2) - centerpoint[0]))
@@ -86,15 +96,15 @@ def load_PSFs_csv(psfs_path, meta_path, img_dims):
     The assumption is that all the PSFs will have a name like F19.csv
     where 19 is the field number. These should correspond to the field numbers in
     the meta file.
-    
+
     The meta file is an excel csv file with the first row:
-    Field Number,X (mm),Y (mm),X image (px),Y image (px) 
-    
+    Field Number,X (mm),Y (mm),X image (px),Y image (px)
+
     Parameters:
         psfs_path: str, path to directory containing all the PSFs
         meta_path: str, path to the meta csv file
         img_dims: a tuple with two elements, signifying the dimensions of the expected image or PSF
-        
+
     Returns:
         psfs: an ndarray of the PSFs.
     """
@@ -104,8 +114,8 @@ def load_PSFs_csv(psfs_path, meta_path, img_dims):
 #         reader = csv.DictReader(file)
 #         for row in reader:
 #             meta_list.append(row)
-    metaman = MetaMan(meta_path)        
-    
+    metaman = MetaMan(meta_path)
+
     # expect a directory with a bunch of PSFs therein as separate csvs;
     # list out all csv file names in the directory
     psf_paths = glob.glob(psfs_path.removesuffix('/') + '/F*')
